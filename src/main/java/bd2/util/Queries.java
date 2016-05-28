730ff0dbfbda177cdf9d4e40e596876957c0fb33
nicolas <nicomanzato@gmail.com>
2016-05-28 19:56:01 -0300

    arreglando esto .com

--- src/main/java/bd2/util/Queries.java
+++ src/main/java/bd2/util/Queries.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import bd2.model.Documento;
+import bd2.model.Moderador;
 
 public class Queries {
 	
@@ -17,14 +19,47 @@ public class Queries {
 		
 		// Listar los nombres de todos los doc
 		
+		Transaction tx = null;
+		
 		System.out.println("Listar los nombres de todos los doc");
 		
 		Configuration cfg = new Configuration();
         cfg.configure("hibernate/hibernate.cfg.xml");
 		sessions = cfg.buildSessionFactory();
-		Session session = sessions.openSession();
-		
+        
+        Session session = sessions.openSession();
+        
+        tx = session.beginTransaction();
+        tx.setTimeout(5);
+        
 		Query query = session.createQuery("from Documento");
+		
+		tx.commit();
+		
+		return query.list();
+		
+	}
+	
+	public static List<Moderador> query_2(){
+		
+		// Listar los emails de los moderadores que hayan evaluado traducciones al ingles
+		
+		Transaction tx = null;
+		
+		System.out.println("Listar los emails de los moderadores que hayan evaluado traducciones al ingles");
+		
+		Configuration cfg = new Configuration();
+        cfg.configure("hibernate/hibernate.cfg.xml");
+		sessions = cfg.buildSessionFactory();
+        
+        Session session = sessions.openSession();
+        
+        tx = session.beginTransaction();
+        tx.setTimeout(5);
+        
+		Query query = session.createQuery("SELECT E FROM Evaluacion as E WHERE E.traduccion.idioma.nombre = 'Ingles'");
+		
+		tx.commit();
 		
 		return query.list();
 		
--- src/main/java/bd2/util/QueriesRunner.java
+++ src/main/java/bd2/util/QueriesRunner.java
@@ -1,14 +1,25 @@
 package bd2.util;
 
 import bd2.model.Documento;
+import bd2.model.Moderador;
 
 public class QueriesRunner {
 	
 	private static void TestQuery_1(){
 		
-		for(Documento d : Queries.query_1()){
+		for(Documento documento : Queries.query_1()){
 			
-			System.out.println("Documento: "+d);
+			System.out.println("Documento: "+documento);
+			
+		}
+		
+	}
+	
+	private static void TestQuery_2(){
+		
+		for(Moderador moderador : Queries.query_2()){
+			
+			System.out.println("Email: "+moderador.getEmail());
 			
 		}
 		
@@ -17,6 +28,8 @@ public class QueriesRunner {
 	public static void main(String[] args) {
 		
 		TestQuery_1();
+		
+		TestQuery_2();
 		
 	}
 
